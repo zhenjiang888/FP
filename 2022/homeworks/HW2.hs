0d855ba4ee78b2ec13b151be3e734da2f3df7c6c
vbcpascal <vbcpascal@outlook.com>
2022-09-14 17:36:31 +0800

    Upload ch2,2

---
+++ 2022/homeworks/HW2.hs
@@ -0,0 +1,33 @@
+module HW2 where
+
+-- Problem #1: 写出 and 函数的三种其他的定义
+and1 :: Bool -> Bool -> Bool
+and1 = _
+
+and2 :: Bool -> Bool -> Bool
+and2 = _
+
+and3 :: Bool -> Bool -> Bool
+and3 = _
+-- End Problem #1
+
+-- Problem #2: 给出函数 div 的一种或多种定义
+div1 :: Integer -> Integer -> Integer
+div1 = _
+-- 如果你认为这个问题无解或者很难，请给出必要的说明。
+-- （请用 undefined 代替上面的下划线，并在下面的括号中填写，不要修改第一行）
+{- Manual #2
+  请在此处填写您的解答。
+-}
+
+-- Problem #3: 写出阶乘函数的其他定义：
+-- Part #3.1: 使用条件方程组
+factGuard :: Integer -> Integer
+factGuard = _
+-- End Part #3.1
+
+-- Part #3.2: 使用分支表达式
+factBranch :: Integer -> Integer
+factBranch = _
+-- End Part #3.2
+-- End Problem #3
