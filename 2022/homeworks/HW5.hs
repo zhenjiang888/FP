ea1c0faa4556a343b836d13afe0923cbb8e366c5
vbcpascal <vbcpascal@outlook.com>
2022-09-23 12:19:40 +0800

    Update assignments

---
+++ 2022/homeworks/HW5.hs
@@ -0,0 +1,48 @@
+module HW5 where
+
+-- Problem #1: define safetail
+-- Part #1.1: use a conditional expression
+safetail1 :: [a] -> [a]
+safetail1 = _
+-- End Part #1.1
+
+-- Part #1.2: use guarded equations
+safetail2 :: [a] -> [a]
+safetail2 = _
+-- End Part #1.2
+
+-- Part #1.3: use pattern matching
+safetail3 :: [a] -> [a]
+safetail3 = _
+-- End Part #1.3
+-- End Problem #1
+
+-- Problem #2: Luhn algorithm
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn = _
+-- End Problem #2
+
+-- Problem #3: Caesar crack
+crack :: String -> String
+crack xs = encode (-factor) xs
+  where factor = position (minimum chitab) chitab
+        chitab = [chisqr (rotate n table') table | n <- [0..25]]
+        table' = freqs xs
+        freqs = _
+        chisqr = _
+-- End Problem #3
+
+-- Problem #4: Pythagorean triples
+pyths :: Int -> [(Int, Int, Int)]
+pyths = _
+-- End Problem #4
+
+-- Problem #5: perfect integers
+perfects :: Int -> [Int]
+perfects = _
+-- End Problem #5
+
+-- Problem #6: scalar product
+scalarProduct :: Num a => [a] -> [a] -> a
+scalarProduct = _
+-- End Problem #6
