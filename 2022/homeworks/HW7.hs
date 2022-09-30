e994a310b259e11639520d43732a24c087eca466
vbcpascal <vbcpascal@outlook.com>
2022-09-30 13:24:47 +0800

    Update HW7

---
+++ 2022/homeworks/HW7.hs
@@ -0,0 +1,29 @@
+module HW7 where
+
+-- Problem #1: multiplies for natural numbers
+data Nat = Zero | Succ Nat
+  deriving (Show)
+
+add :: Nat -> Nat -> Nat
+add Zero     n = n
+add (Succ m) n = Succ (add m n)
+
+multiplies :: Nat -> Nat -> Nat
+multiplies = _
+-- End Problem #1
+
+-- Problem #2: folde for Exprs
+data Expr
+  = Val Int
+  | Add Expr Expr
+  | Mul Expr Expr
+  deriving (Show)
+
+-- try to figure out the suitable type yourself
+folde :: _
+folde = _
+-- End Problem #2
+
+-- Problem #3: recursive tree type
+data Tree a = _
+-- End Problem #3
