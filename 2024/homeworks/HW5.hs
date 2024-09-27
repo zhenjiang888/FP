2028b18f13b99b92623e58a1cac14143188c0f05
CudraniaTrec <cudraniatrec@163.com>
2024-09-27 14:30:09 +0800

    Updating 2024 Fall ch06

--- 2024/homeworks/HW5.hs
+++ 2024/homeworks/HW5.hs
@@ -1,6 +1,5 @@
 module HW5 where
 
-import Prelude hiding (and, concat, replicate, (!!), elem, filter, map)
 import Data.Char (chr, isLower, ord)
 
 -- **=========[ Ch.05 ]=========**
@@ -30,8 +29,29 @@ luhn = _
 -- **=========[ Ch.06 ]=========**
 
 -- Problem #1: Caesar crack
+encode :: Int -> String -> String 
+encode n xs = [ shift n x | x <- xs ] 
+
+shift :: Int -> Char -> Char 
+shift n c | isLower c  =  int2let $ mod (let2int c + n) 26
+          | otherwise  =  c 
+
+let2int :: Char -> Int 
+let2int c = ord c - ord 'a' 
+int2let :: Int -> Char 
+int2let n = chr $ ord 'a' + n
+        
+table :: [Float] 
+table = [ 8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0, 
+          0.2, 0.8, 4.0, 2.4,  6.7, 7.5, 1.9, 0.1, 6.0, 
+          6.3, 9.0, 2.8, 1.0,  2.4, 0.2, 2.0, 0.1 ] 
+
 crack :: String -> String
-crack = _
+crack xs  =  encode (-factor) xs 
+    where   
+        factor = position (minimum chitab) chitab      
+        chitab = [ chisqr (rotate n table') table | n <- [0..25] ]
+        table' = freqs xs
 
 freqs :: String -> [Float]
 freqs = _
