b0f835604a54099427d6a8df9ea5ab6ead8e73ed
CudraniaTrec <cudraniatrec@163.com>
2024-10-18 13:17:27 +0800

    Updating 2024 Fall ch10

---
+++ 2024/homeworks/HW8.hs
@@ -0,0 +1,88 @@
+module HW8 where
+
+-- Ch.10
+
+-- 为简便起见，我们不允许任何中间结果超出 2^32。
+-- 这意味着可以提前对搜索进行剪枝：
+-- 1. 任何结果均不允许超过 2^32。
+-- 2. 任何指数均不允许超过 32。
+-- 在大家的 64 位系统上，GHC 一般把 Int 实现为 64 位整数，因此
+-- 只要严格执行上述剪枝策略，就不必担心运算溢出问题。
+
+data Op
+  = Add
+  | Sub
+  | Mul
+  | Div
+  | Pow -- 添加的指数运算
+  deriving Eq
+
+data Expr
+  = Val Int
+  | App Op Expr Expr
+  deriving Eq
+
+-- 一些辅助函数
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = x `div` y
+apply Pow x y = x ^ y
+
+valid :: Op -> Int -> Int -> Bool
+valid = _
+
+eval :: Expr -> [Int]
+eval (Val n) = [n | n > 0]
+eval (App o l r) = [apply o x y | x <- eval l, y <- eval r, valid o x y]
+
+subs :: [a] -> [[a]]
+subs [] = [[]]
+subs (x:xs) = subs xs ++ map (x:) (subs xs)
+
+interleave :: a -> [a] -> [[a]]
+interleave x [] = [[x]]
+interleave x (y:ys) = (x:y:ys) : map (y:) (interleave x ys)
+
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms (x:xs) = concatMap (interleave x) (perms xs)
+
+choices :: [a] -> [[a]]
+choices = concatMap perms . subs
+
+split :: [a] -> [([a], [a])]
+split [] = []
+split [_] = []
+split (x:xs) = ([x], xs) : [(x:ls, rs) | (ls, rs) <- split xs]
+
+-- 你需要完成下面的 solutions 函数
+-- 你可能还需要自定义一些辅助函数或类型
+solutions :: [Int] -> Int -> [Expr]
+solutions = _
+
+-- 下面是我们为 Expr 和 Op 提供的一个 Show 的实现
+-- 这并不是本次作业必需的，但是在调试过程中可能会让表达式更易读
+-- 这个实现使用了 showsPrec，有关它的细节你可以参考相关文档：
+-- https://hackage.haskell.org/package/base-4.15.0.0/docs/Text-Show.html#t:Show
+-- 以及 Haskell 2010 Report 的 11.4 节：
+-- https://www.haskell.org/onlinereport/haskell2010/haskellch11.html
+
+instance Show Op where
+  show Add = "+"
+  show Sub = "-"
+  show Mul = "*"
+  show Div = "/"
+  show Pow = "^"
+
+instance Show Expr where
+  showsPrec _ (Val n) = shows n
+  showsPrec p (App op x y)
+    = showParen (p > q)
+    $ showsPrec q x . showChar ' ' . shows op
+    . showChar ' ' . showsPrec (succ q) y
+    where q = case op of
+            Add -> 6; Sub -> 6
+            Mul -> 7; Div -> 7
+            Pow -> 8
