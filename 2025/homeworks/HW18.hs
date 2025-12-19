c3a14f56a43dbdcd0c0451ef8d714daef7edf451
TRCYX <trcyxs@gmail.com>
2025-12-19 13:30:59 +0800

    2025 Fall ch20

--- 2025/homeworks/HW18.hs
+++ 2025/homeworks/HW18.hs
@@ -4,3 +4,10 @@ module HW18 where
 lsp :: (a -> Bool) -> [a] -> [a]
 lsp = _
 
+-- Ch24: BMF2-3
+minimax :: [[Int]] -> Int
+minimax = _
+
+-- Ch25: BMF3-2
+tailsums :: (Num a) => [a] -> [a]
+tailsums = _
--- 2025/homeworks/HW19.hs
+++
@@ -1,7 +0,0 @@
-module HW19 where
-
--- Ch25: BMF3-2
-tailsums :: (Num a) => [a] -> [a]
-tailsums = _
-
-
