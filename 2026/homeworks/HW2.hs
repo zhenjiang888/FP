4778bef637d6ceabb0c3332d4ec25d0470cd0d59
TRCYX <trcyxs@gmail.com>
2026-09-17 23:16:44 +0800

    Update 2026 fall ch3

--- 2026/homeworks/HW2.hs
+++ 2026/homeworks/HW2.hs
@@ -14,6 +14,9 @@ and3 = _
 -- Problem #2: 给出函数 div 的一种或多种定义
 div1 :: Integer -> Integer -> Integer
 div1 = _
+
+-- 如果你认为这个问题无解或很难，请在代码中展示你努力尝试得到的结果，并以注释形
+-- 式描述你遇到的困难。如果你认真做到了这些，你的本次作业分数并不会扣减。
 -- End Problem #2
 
 -- Problem #3: 写出阶乘函数的其他定义：
@@ -27,5 +30,3 @@ factBranch :: Integer -> Integer
 factBranch = _
 -- End Part #3.2
 -- End Problem #3
-
--- Problem #4: 请先自行思考，不必提交
