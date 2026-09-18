98784d3c5cbd195fc6b69b422e958c90ee819913
TRCYX <trcyxs@gmail.com>
2026-09-18 12:41:09 +0800

    2026 fall ch3

---
+++ 2026/homeworks/HW3.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import System.Random
+
+-- Problem #4: 猜数字小游戏
+main :: IO ()
+main = _
+
+-- 如果仅在编写程序部分，你遇到极大困难，导致难以完成，请在代码中展示你努力尝试
+-- 得到的结果，并以注释形式描述你遇到的困难。如果你认真做到了这些，你的本次作业分数并不会扣减。
+
+-- End Problem #4
